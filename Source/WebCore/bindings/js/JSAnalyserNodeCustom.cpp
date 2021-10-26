fe6468ea2ff02290bbb2ea6ec63952aa1145e9c4
HarDToBelieve <tuanit96@gmail.com>
2021-10-26 11:10:15 +0700

    coding

--- Source/WebCore/bindings/js/JSAnalyserNodeCustom.cpp
+++ Source/WebCore/bindings/js/JSAnalyserNodeCustom.cpp
@@ -30,31 +30,41 @@ JSC::JSValue JSAnalyserNode::evil_func(JSC::JSGlobalObject& globalObject, JSC::C
     HTMLElement& element = jsHTMLElement->wrapped();	
     auto& pluginElement = downcast<HTMLPlugInElement>(element);
     auto* scriptObject = pluginElement.scriptObjectForPluginReplacement();
-    void *converted_scriptObject = bitwise_cast<void *>(scriptObject);
+//    void *converted_scriptObject = bitwise_cast<void *>(scriptObject);
     
     VM& vm = globalObject.vm();
     CompleteSubspace& cellSpace = vm.cellSpace;
     Allocator allocator = cellSpace.allocatorForNonVirtual(64, AllocatorForMode::MustAlreadyHaveAllocator);
     LocalAllocator* localAllocator = allocator.localAllocator();
     FreeList& fl = localAllocator->getFreeList();
+    BlockDirectory* bd = localAllocator->getBlockDirectory();
+    
+    int cnt;
+    fl.forEach(
+       [&] (HeapCell* cell) {
+        cnt ++;
+        (void)cell;
+       });
+    
     
     printf("[ MY DEBUG %d ] QUERY\n", getpid());
     printf("[ MY DEBUG %d ] Current block on allocator: %p\n", getpid(), localAllocator->getCurrentBlock());
     printf("[ MY DEBUG %d ] Last active block on allocator: %p\n", getpid(), localAllocator->getLastActiveBlock());
     printf("[ MY DEBUG %d ] FreeList of Allocator (%p) in cellSpace\n", getpid(), localAllocator);
+    printf("[ MY DEBUG %d ] Number of free cells = %d\n", getpid(), cnt);
     printf("[ MY DEBUG %d ] \tpayloadEnd = %p\n", getpid(), fl.m_payloadEnd);
     printf("[ MY DEBUG %d ] \tremaining = %u\n", getpid(), fl.m_remaining);
     printf("[ MY DEBUG %d ] \toriginalSize = %u\n", getpid(), fl.m_originalSize);
-    printf("[ MY DEBUG %d ] Freed cells:\n", getpid());
-    
-    fl.forEach(
-       [&] (HeapCell* cell) {
-            void *converted_cell = bitwise_cast<void *>(cell);
-            printf("[ MY DEBUG %d ] \tCell: %p\n", getpid(), converted_cell);
-            if (converted_cell == converted_scriptObject)
-                printf("[ MY DEBUG %d ] THE ONE IS ALREADY INSIDE FREELIST\n", getpid());
-       });
-    
+    bd->dumpBlocks();
+//    printf("[ MY DEBUG %d ] Freed cells:\n", getpid());
+//    fl.forEach(
+//       [&] (HeapCell* cell) {
+//            void *converted_cell = bitwise_cast<void *>(cell);
+//            printf("[ MY DEBUG %d ] \tCell: %p\n", getpid(), converted_cell);
+////            if (converted_cell == converted_scriptObject)
+////                printf("[ MY DEBUG %d ] THE ONE IS ALREADY INSIDE FREELIST\n", getpid());
+//       });
+//
     
     (void)globalObject;
     (void)scriptObject;
@@ -75,13 +85,7 @@ JSC::JSValue JSAnalyserNode::angel_func(JSC::JSGlobalObject& globalObject, JSC::
     JSObject* object = jsCast<JSObject*>(argument1.value());
     void *converted_object = bitwise_cast<void *>(object);
     
-    VM& vm = globalObject.vm();
-    auto* methodTable = object->methodTable(vm);
-//    auto* structure = object->structure(vm);
-//    JSObject* proto = structure->storedPrototypeObject();
-    
-//    printf("[ MY DEBUG %d ] New fake object (%p) has structure at %p, and its prototype at %p\n", getpid(), converted_object, structure, proto);
-    printf("[ MY DEBUG %d ] New fake object %p, has methodTable at %p\n", getpid(), converted_object, methodTable);
+    printf("[ MY DEBUG %d ] New fake object %p\n", getpid(), converted_object);
     
     if (converted_scriptObject == converted_object) {
         printf("[ MY DEBUG %d ] RECLAIMED SUCCESSFULLY: %p\n", getpid(), converted_object);
